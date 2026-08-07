85cf18fa666acba8f0c97d0a79a79d2a56dc5b8c
José García 2026 <josegarcia19809@gmail.com>
2026-08-06 18:07:51 -0600

    Aprendiendo: Get con datos

--- inicio/main.py
+++ inicio/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="MINI BLOG")
+
+BLOG_POST = [
+    {
+        "id": 1,
+        "title": "Introducción a Python",
+        "content": "Python es un lenguaje de programación fácil de aprender."
+    },
+    {
+        "id": 2,
+        "title": "Uso de Diccionarios",
+        "content": "Los diccionarios almacenan datos en pares clave-valor."
+    },
+    {
+        "id": 3,
+        "title": "Listas en Python",
+        "content": "Las listas permiten almacenar colecciones ordenadas de elementos."
+    }
+]
+
 
 @app.get("/")
-def read_root():
+def home():
     return {"Hola": "Mundo"}
+
+@app.get("/posts")
+def list_posts():
+    return {"data": BLOG_POST}
